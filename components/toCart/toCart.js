985361c0409565e6908d9dcb94e34a27d333ca2e
JY <154524528@qq.com>
2021-03-03 17:00:39 +0800

    购物车动画 优化gitflow-feature-stash: 1_1_0_cd_114

---
+++ components/toCart/toCart.js
@@ -0,0 +1,44 @@
+Component({
+  data: {
+    finger: 0,
+    linePos: []
+  },
+  methods: {
+    start: function (params) {
+      let proId = params.proid;
+      let img = params.img;
+      this.setData({
+        'img':img
+      })
+      // params.position.linePos
+      this.linePos = params.position.linePos;
+      this.startAnimation();
+    },
+    startAnimation: function () {
+      this.setData({
+        hide_good_box: false
+      })
+      console.log('startAnimation')
+      let index = 0,
+        bezier_points = this.linePos['bezier_points'];
+      let timeT = 0;
+      var that = this;
+      timeT = setInterval(function () {
+        index++;
+        if (index >= 30) {
+          that.setData({
+            hide_good_box: true,
+            bus_x:1000,
+            bus_y:1000
+          })
+          clearInterval(timeT);         
+        }
+        that.setData({
+          bus_x: bezier_points[index]['x'],
+          bus_y: bezier_points[index]['y']
+        })
+      }, 20);
+      return false;
+    },
+  }
+})
